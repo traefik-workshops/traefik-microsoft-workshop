278147fbafa1b7887ebf7212eacf8415315930a2
maythamalfouadi <57093866+maythamalfouadi@users.noreply.github.com>
2024-08-22 03:57:38 -0700

    updating traffic

--- module-4/traffic/scripts/traffic.sh
+++ module-4/traffic/scripts/traffic.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-GW_DOMAIN=$(cat /var/shared/apigw)
+#GW_DOMAIN="exclusive-quokka-s9q2my.b0mlgjly.traefikhub.io"
 
 random() {
-	NUMBER=$RANDOM
-	let "NUMBER%=$1"
+        NUMBER=$RANDOM
+        let "NUMBER%=$1"
 
-	echo $NUMBER
+        echo $NUMBER
 }
 
 call() {
@@ -21,21 +21,19 @@ call() {
    # add 10 to have a minimum of 10 for the -c option of hey
    let "NB+=10"
    hey -m "$METHOD" -c 10 -n $NB \
-  	-H 'accept: application/json' \
-  	-H "Authorization: Bearer $TOKEN" "https://$GW_DOMAIN$HPATH" | grep responses
+        -H 'accept: application/json' \
+        -H "Authorization: Bearer $TOKEN" "https://$GW_DOMAIN$HPATH" | grep responses
    echo ""
 
 }
 
 for (( ; ; ))
 do
- call 'GET' "/customers/customers" 1000 2
- call 'GET' "/customers-versioned/v2/customers" 1000 2
- call 'GET' "/customers-versioned/customers?v=3" 1000 2
- call 'GET' "/flights/flights" 1000 5
- call 'GET' "/tickets/tickets" 1000 2
- call 'POST' "/tickets/tickets" 1000 2
- call 'POST' "/employees/employees" 1000 2
+ call 'GET' "/customers" 1000 2
+ call 'GET' "/flights" 1000 5
+ call 'GET' "/tickets" 1000 2
+ call 'POST' "/tickets" 1000 2
+ call 'POST' "/employees" 1000 2
 
  sleep 2;
 done;
